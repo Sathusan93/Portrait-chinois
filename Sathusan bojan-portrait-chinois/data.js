4f2ebc51f32f2021bdf2d0163b4b419e532b1e4b
Sathusan93 <158066257+Sathusan93@users.noreply.github.com>
2024-02-14 16:17:25 +0100

    Add files via upload

---
+++ Sathusan bojan-portrait-chinois/data.js
@@ -0,0 +1,62 @@
+
+var data = [
+    {
+      analogie: "une couleur",
+      valeurAnalogie: "la couleur bleu ",
+      description:"Car c'est ma couleur préféré.Le bleu représente l'apaisement  et la loyauté.Le bleu est également associer au bien-être et à la spiritualité.",
+      image:"img/couleur bleu.jpg",
+    },
+  
+    {
+      analogie: "un animal",
+      valeurAnalogie: "une chouette",
+      description:
+        "La chouette est un oiseau nocturne qui  symbolise la sagesse, le mystère et l'intelligence.Elle chasse principalement la nuit et émet souvent un hululement distinctif.",
+      image: "img/chouette.webp",
+    },
+  
+    {
+      analogie: "une voiture",
+      valeurAnalogie: "une BMW M5",
+      description:
+        "La BMW M5 est une de mes voiture de rêve.La BMW M5 est une berline sportive de luxe très belle et connue pour son comfort intérieur et un design élégant.",
+     image:"img/bmw m5 .avif"
+   
+    },
+  
+    {
+      analogie: "un film",
+      valeurAnalogie: "Spiderman",
+      description:
+        "C'est mon film et super-héros préféré.Spider-Man est un super-héros emblématique qui représente le courage et la capacité de surmonter les défis.Il symbolise pour moi, la capacité de transformer les obstacles en opportunuité et de puiser dans sa force intérieur pour surmonter les défis.",
+      image: "img/spiderman.jpg",
+    },
+
+    {
+      analogie: "une ville",
+      valeurAnalogie: "New York",
+      description:
+        "La ville de NYC est une des ville que j'aimerais visiter de nuit aux États-Unis.NYC est une ville très animé la nuit comme les néons brillant de Times Square et les rue avec des panneaux d'affiches,de taxi et plein de chose iconique de NYC.",
+      image: "img/NYC.jpg",
+  
+    },
+  
+    {
+      analogie: "un sport",
+      valeurAnalogie: "le football",
+      description:
+        "Le football est mon sport préféré,mon joueur de football préféré est Kylian Mbappé.Un joueur de football rapide et très fort.",
+      image: "img/foot.jpg",
+  
+    },
+  
+    {
+      analogie: "un fruit",
+      valeurAnalogie: "une grenade",
+      description:
+        "La grenade est mon fruit préféré.Tout comme la grenade,je suis à la fois complexe et riche en facettes comme les graines juteuse qui se trouvent dans ce fruit.",
+      image: "img/grenade.jpg",
+     
+    },
+  
+  ];
